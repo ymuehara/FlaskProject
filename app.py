54b374529031827d1f3112a33599b1a47bec60b7
ymuehara <yuriuehara@hotmail.com>
2020-10-30 23:46:04 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World"}
+
+    def post(self):
+        return {"data": "Posted"}
+
+
+api.add_resource(HelloWorld, "/helloworld")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
