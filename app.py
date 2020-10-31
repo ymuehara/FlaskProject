2ceee22726e76fcf680b27a5d0ddcc15f2353a19
ymuehara <yuriuehara@hotmail.com>
2020-10-31 19:11:10 -0300

    updated app and test

--- app.py
+++ app.py
@@ -5,15 +5,29 @@ app = Flask(__name__)
 api = Api(app)
 
 
+names = {"yuri": {"age": 30, "gender": "female"},
+         "eric": {"age": 31, "gender": "male"}
+         }
+
+videos = {}
+
+
+class Video(Resource):
+    def get(selfself, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        return {}
+
+api.add_resource(Video, '/video/<int:video_id>')
+
+
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
-
-    def post(self):
-        return {"data": "Posted"}
+    def get(self, name):
+        return names[name]
 
 
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(f"{BASE}helloworld")
+response = requests.put(f"{BASE}helloworld/yuri", {"likes": 10})
 print(response.json())
