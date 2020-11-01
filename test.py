d4c51a926a7f60d96bed5c6b89039237d8c48e50
ymuehara <yuriuehara@hotmail.com>
2020-11-01 18:53:30 -0300

    updated with database config and serializing objects

--- test.py
+++ test.py
@@ -3,16 +3,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"likes": 53, "name": "Yuri", "views": 1345},
-        {"likes": 12, "name": "Pancake with blueberries", "views": 567},
-        {"likes": 34, "name": "Maple likes cake", "views": 8753}
-        ]
-for i in range(len(data)):
-    response = requests.put(f"{BASE}video/{i}", data[i])
-    print(response.json())
-input()
-response = requests.delete(f"{BASE}video/0")
-print(response)
-input()
-response = requests.get(f"{BASE}video/2")
+# data = [{"likes": 53, "name": "Yuri", "views": 1345},
+#         {"likes": 12, "name": "Pancake with blueberries", "views": 567},
+#         {"likes": 34, "name": "Maple likes cake", "views": 8753}
+#         ]
+#
+# for i in range(len(data)):
+#     response = requests.put(f"{BASE}video/{i}", data[i])
+#     print(response.json())
+
+response = requests.patch(f"{BASE}video/2", {"views": 99})
 print(response.json())
