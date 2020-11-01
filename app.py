90dedf2070aefb3522bd9b73bf7a1995e8375b3e
ymuehara <yuriuehara@hotmail.com>
2020-10-31 23:52:43 -0300

    updated with videos put and delete requests

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-
-names = {"yuri": {"age": 30, "gender": "female"},
-         "eric": {"age": 31, "gender": "male"}
-         }
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=str, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=str, help="Likes of the video", required=True)
 
 videos = {}
 
 
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="video id is not valid :(")
+
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(400, message="video already exists with that id :(")
+
 class Video(Resource):
-    def get(selfself, video_id):
+    def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
-        return {}
+        abort_if_video_exists(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201 #created
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
 api.add_resource(Video, '/video/<int:video_id>')
-
-
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
-
-
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 
 if __name__ == '__main__':
